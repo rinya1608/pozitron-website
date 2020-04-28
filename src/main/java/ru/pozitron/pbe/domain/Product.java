9a7bbfec1d4d1810c4b5f08d2ed498d3af5d1f48
Renat <rinya1608@mail.ru>
2020-04-28 20:58:27 +0500

    add entity for product

---
+++ src/main/java/ru/pozitron/pbe/domain/Product.java
@@ -0,0 +1,106 @@
+package ru.pozitron.pbe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer sale;
+    private Double count;
+    private String unit;
+    private String fotoname;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Category category;
+
+    public Product() {
+    }
+
+
+
+    public Product(String name, String description, Double price, Double count, String unit, Category category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.count = count;
+        this.unit = unit;
+        this.category = category;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getSale() {
+        return sale;
+    }
+
+    public void setSale(Integer sale) {
+        this.sale = sale;
+    }
+
+    public Double getCount() {
+        return count;
+    }
+
+    public void setCount(Double count) {
+        this.count = count;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getFotoname() {
+        return fotoname;
+    }
+
+    public void setFotoname(String fotoname) {
+        this.fotoname = fotoname;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
