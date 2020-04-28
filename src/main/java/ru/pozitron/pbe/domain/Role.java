12a8b67c2b2e6ee6c1c14b62c447332e387e4e86
Renat <rinya1608@mail.ru>
2020-04-28 20:58:08 +0500

    add role

---
+++ src/main/java/ru/pozitron/pbe/domain/Role.java
@@ -0,0 +1,14 @@
+package ru.pozitron.pbe.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER,
+    MODERATOR,
+    ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
