71d36c85ac34e3e5eccfb1b186f3d90b69a5cfb6
Renat <rinya1608@mail.ru>
2020-04-28 21:17:06 +0500

    add mapping for registration page

---
+++ src/main/java/ru/pozitron/pbe/controller/RegistrationController.java
@@ -0,0 +1,44 @@
+package ru.pozitron.pbe.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.pozitron.pbe.domain.User;
+import ru.pozitron.pbe.service.UserService;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Model model){
+
+        if (!userService.addUser(user)){
+            model.addAttribute("message","Пользователь уже зарегестрирован");
+            return "regestration";
+        }
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+        if (isActivated){
+            model.addAttribute("message","Аккаунт активирован");
+        }
+        else {
+            model.addAttribute("message","Код активации не найден");
+        }
+        return "login";
+    }
+
+}
