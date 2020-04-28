80f152ba79656996f8509a718814d7d37076a114
Renat <rinya1608@mail.ru>
2020-04-28 21:14:35 +0500

    add mapping for main page

---
+++ src/main/java/ru/pozitron/pbe/controller/MainController.java
@@ -0,0 +1,23 @@
+package ru.pozitron.pbe.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.pozitron.pbe.domain.Category;
+import ru.pozitron.pbe.repository.CategoryRepository;
+
+@Controller
+public class MainController {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @GetMapping("/")
+    public String viewCategory(Model model){
+        Iterable<Category> categories = categoryRepository.findAllByParentCategory(null);
+        model.addAttribute("categories",categories);
+        return "index";
+    }
+
+
+}
