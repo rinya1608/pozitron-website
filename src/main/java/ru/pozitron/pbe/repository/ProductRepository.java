26c45966006adae526c4c0e28c2685252c4b92d0
Renat <rinya1608@mail.ru>
2020-04-28 21:01:37 +0500

    add repository for product

---
+++ src/main/java/ru/pozitron/pbe/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package ru.pozitron.pbe.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.CrudRepository;
+import ru.pozitron.pbe.domain.Category;
+import ru.pozitron.pbe.domain.Product;
+
+import org.springframework.data.domain.Pageable;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    Page<Product> findByCategory(Category category, Pageable pageable);
+    Page<Product> findAll(Pageable pageable);
+}
