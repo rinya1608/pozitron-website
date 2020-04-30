80a471428c711144ddf484bbc33a8863b2cd600d
Renat <rinya1608@mail.ru>
2020-05-01 02:29:03 +0500

    Add find by email and find by username like

--- src/main/java/ru/pozitron/pbe/repository/UserRepository.java
+++ src/main/java/ru/pozitron/pbe/repository/UserRepository.java
@@ -5,6 +5,7 @@ import ru.pozitron.pbe.domain.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String username);
-
+    User findByEmail(String email);
     User findByActivationCode(String code);
+    User findByUsernameLike(String username);
 }
