e4744774ec8f7b4a62a7618ad3f966767d95185f
Renat <rinya1608@mail.ru>
2020-05-01 02:27:56 +0500

    Add mapping for userProfile Page
    In GetMapping configure sending data of an authorized user to a page.
    In PostMapping configure changes to the user's personal data in the profile.

--- src/main/java/ru/pozitron/pbe/controller/UserController.java
+++ src/main/java/ru/pozitron/pbe/controller/UserController.java
@@ -2,12 +2,16 @@ package ru.pozitron.pbe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.pozitron.pbe.domain.Category;
 import ru.pozitron.pbe.domain.Role;
 import ru.pozitron.pbe.domain.User;
+import ru.pozitron.pbe.repository.CategoryRepository;
 import ru.pozitron.pbe.repository.UserRepository;
+import ru.pozitron.pbe.service.UserService;
 
 import java.util.Arrays;
 import java.util.Map;
@@ -16,23 +20,29 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String userList(Model model){
         model.addAttribute("users",userRepository.findAll());
         return "userList";
     }
     @GetMapping("{user}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String userEdit(@PathVariable User user,Model model){
         model.addAttribute("user",user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String,String> form,
@@ -51,5 +61,30 @@ public class UserController {
         }
         userRepository.save(user);
         return "redirect:/user";
+    }
+    @GetMapping("/profile")
+    public String userProfile(@AuthenticationPrincipal User user,Model model){
+        Iterable<Category> categories = categoryRepository.findAllByParentCategory(null);
+        model.addAttribute("user",user);
+        model.addAttribute("categories",categories);
+        return "userProfile";
+    }
+    @PostMapping("/profile")
+    public String updateUserProfile(@AuthenticationPrincipal User user,
+                                    @RequestParam(required = false) String name,
+                                    @RequestParam(required = false) String username,
+                                    @RequestParam(required = false) String email,
+                                    @RequestParam(required = false) String number,
+                                    Model model){
+
+        if (name != null) model.addAttribute("nameMessage",userService.updateUserName(user,name));
+        if (username != null) model.addAttribute("usernameMessage",userService.updateUserUsername(user,username));
+        if (email != null) model.addAttribute("emailMessage",userService.updateUserEmail(user,email));
+        if (number != null) model.addAttribute("numberMessage",userService.updateUserNumber(user,number));
+        userRepository.save(user);
+        Iterable<Category> categories = categoryRepository.findAllByParentCategory(null);
+        model.addAttribute("user",user);
+        model.addAttribute("categories",categories);
+        return "userProfile";
     }
 }
