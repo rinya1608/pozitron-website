33612e70aa9ba50c96915db5c06033a8307030ed
Renat <rinya1608@mail.ru>
2020-05-13 22:30:58 +0500

    add globally pass to the model categories

--- src/main/java/ru/pozitron/pbe/controller/MainController.java
+++ src/main/java/ru/pozitron/pbe/controller/MainController.java
@@ -2,22 +2,24 @@ package ru.pozitron.pbe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import ru.pozitron.pbe.domain.Category;
 import ru.pozitron.pbe.repository.CategoryRepository;
 
 @Controller
+@ControllerAdvice
 public class MainController {
     @Autowired
     CategoryRepository categoryRepository;
-
-    @GetMapping("/")
-    public String viewCategory(Model model){
-        Iterable<Category> mainCategories = categoryRepository.findAllByParentCategory(null);
-        model.addAttribute("mainCategories",mainCategories);
-        return "index";
+    @ModelAttribute("mainCategories")
+    public Iterable<Category> Category(){
+        return categoryRepository.findAllByParentCategory(null);
     }
 
-
+    @GetMapping("/")
+    public String main(){
+        return "index";
+    }
 }
