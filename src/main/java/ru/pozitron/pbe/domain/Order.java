45973b7c7012b3dfa5d611aa9024003e06f70d7a
Renat <rinya1608@mail.ru>
2020-06-18 21:35:45 +0500

    add entity for order

---
+++ src/main/java/ru/pozitron/pbe/domain/Order.java
@@ -0,0 +1,34 @@
+package ru.pozitron.pbe.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany
+    private Set<Cart> carts;
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Cart> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(Set<Cart> carts) {
+        this.carts = carts;
+    }
+}
