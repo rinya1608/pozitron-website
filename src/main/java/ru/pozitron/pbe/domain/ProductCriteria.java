276df802f9ad4980ebb9a6bbaf6d213d9f250841
Renat <rinya1608@mail.ru>
2020-06-18 21:40:15 +0500

    add class for working with ajax

---
+++ src/main/java/ru/pozitron/pbe/domain/ProductCriteria.java
@@ -0,0 +1,22 @@
+package ru.pozitron.pbe.domain;
+
+public class ProductCriteria {
+    private String name;
+    private Integer quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
