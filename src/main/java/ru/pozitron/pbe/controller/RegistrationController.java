ea1c9c4ba764f301d8fafe217400141bf59956f1
rinya <rinya1608@mail.ru>
2020-08-22 00:35:21 +0500

    add validation for user

--- src/main/java/ru/pozitron/pbe/controller/RegistrationController.java
+++ src/main/java/ru/pozitron/pbe/controller/RegistrationController.java
@@ -3,11 +3,15 @@ package ru.pozitron.pbe.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.pozitron.pbe.domain.User;
 import ru.pozitron.pbe.service.UserService;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class RegistrationController {
@@ -19,7 +23,13 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Model model){
+    public String addUser(@Valid User user, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+            model.addAttribute("validErrors",errors.values());
+
+            return "registration";
+        }
 
         if (!userService.addUser(user)){
             model.addAttribute("message","Пользователь уже зарегестрирован");
