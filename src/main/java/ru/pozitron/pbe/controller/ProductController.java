a74cb5479c78ec29d0ce52d6207fcfa5215c1226
rinya <rinya1608@mail.ru>
2020-08-22 00:27:26 +0500

    add support for file

--- src/main/java/ru/pozitron/pbe/controller/ProductController.java
+++ src/main/java/ru/pozitron/pbe/controller/ProductController.java
@@ -1,6 +1,7 @@
 package ru.pozitron.pbe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -11,26 +12,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.pozitron.pbe.domain.Category;
 import ru.pozitron.pbe.domain.Product;
 import ru.pozitron.pbe.repository.CategoryRepository;
 import ru.pozitron.pbe.repository.ProductRepository;
 
 import org.springframework.data.domain.Pageable;
+import ru.pozitron.pbe.service.ProductService;
 
 import java.math.BigDecimal;
 
 @Controller
 public class ProductController {
     @Autowired
-    ProductRepository productRepository;
+    private ProductRepository productRepository;
     @Autowired
-    CategoryRepository categoryRepository;
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductService productService;
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("products/{category}")
-    public String productList(@PathVariable Category category,
+    public String viewProductList(@PathVariable Category category,
                               Model model,
-                              @PageableDefault(sort = {"id"},direction = Sort.Direction.DESC,size = 1) Pageable pageable){
+                              @PageableDefault(sort = {"id"},direction = Sort.Direction.DESC,size = 3) Pageable pageable){
         Page<Product> page;
         page = productRepository.findByCategory(category,pageable);
         model.addAttribute("page",page);
@@ -39,7 +46,7 @@ public class ProductController {
         return "productList";
     }
     @GetMapping("product/{category}/{product}")
-    public String productList(@PathVariable Product product,Model model){
+    public String viewProductPage(@PathVariable Product product,Model model){
         model.addAttribute("product",product);
         return "productPage";
     }
@@ -56,15 +63,23 @@ public class ProductController {
     @PostMapping("/product/add")
     @PreAuthorize("hasAuthority('ADMIN')")
     public String productSave(@RequestParam String productName,
-                             @RequestParam String description,
-                             @RequestParam BigDecimal price,
-                             @RequestParam Double count,
-                             @RequestParam String unit,
-                             @RequestParam("categoryId")  Category category,
+                              @RequestParam String description,
+                              @RequestParam BigDecimal price,
+                              @RequestParam Integer discountPercent,
+                              @RequestParam Double count,
+                              @RequestParam String unit,
+                              @RequestParam("categoryId")  Category category,
+                              @RequestParam("file") MultipartFile file,
                               Model model){
 
         Product product = new Product(productName,description,price,count,unit,category);
-
+        if (!file.isEmpty()) {
+            product.setFilename(productService.uploadFile(file,uploadPath));
+        }
+        if (discountPercent != null && discountPercent != 0){
+            product.setDiscountPercent(discountPercent);
+            product.setPriceWithDiscount(productService.calculatePriceWithDiscount(price,discountPercent));
+        }
         productRepository.save(product);
         Iterable<Category> categories = categoryRepository.findAll();
         model.addAttribute("categories",categories);
