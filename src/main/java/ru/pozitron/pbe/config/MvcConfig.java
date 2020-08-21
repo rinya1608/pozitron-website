6ebb3adfee094fdee3a87b81a033df4cbd002b57
rinya <rinya1608@mail.ru>
2020-08-22 00:15:59 +0500

    add resource handler for file

--- src/main/java/ru/pozitron/pbe/config/MvcConfig.java
+++ src/main/java/ru/pozitron/pbe/config/MvcConfig.java
@@ -1,5 +1,6 @@
 package ru.pozitron.pbe.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -7,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
@@ -16,5 +20,9 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file://" + uploadPath + "/");
     }
+
+
 }
