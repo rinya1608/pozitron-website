0a560e760fd081e7efaae7ffd71e6220e1cf349e
rinya <rinya1608@mail.ru>
2020-08-22 00:15:22 +0500

    add mapping for search and model attribute for products

--- src/main/java/ru/pozitron/pbe/controller/MainController.java
+++ src/main/java/ru/pozitron/pbe/controller/MainController.java
@@ -1,25 +1,55 @@
 package ru.pozitron.pbe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import ru.pozitron.pbe.domain.Category;
+import ru.pozitron.pbe.domain.Product;
 import ru.pozitron.pbe.repository.CategoryRepository;
+import ru.pozitron.pbe.repository.ProductRepository;
+import ru.pozitron.pbe.service.ProductService;
+
+import java.util.ArrayList;
 
 @Controller
 @ControllerAdvice
 public class MainController {
     @Autowired
     CategoryRepository categoryRepository;
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    ProductService productService;
+
     @ModelAttribute("mainCategories")
     public Iterable<Category> Category(){
         return categoryRepository.findAllByParentCategory(null);
     }
 
+    @GetMapping("/search")
+    public String viewSearchList(@RequestParam(required = false) String productName,
+                                 Model model,
+                                 @PageableDefault(sort = {"name"},direction = Sort.Direction.DESC,size = 3) Pageable pageable){
+        Page<Product> page;
+        page = productRepository.findByNameLike("%" + productName + "%",pageable);
+        model.addAttribute("page",page);
+        model.addAttribute("url","/search");
+        return "SearchList";
+    }
+    @PostMapping("search")
+    public String searchProduct(@RequestParam String name){
+        return "redirect:/search?productName=" + name;
+    }
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model){
+        ArrayList<Product> products = productService.productsByDiscountWithLimit(productRepository.findAllByDiscountPercentNotNull(),9);
+        model.addAttribute("products",products);
         return "index";
     }
 }
