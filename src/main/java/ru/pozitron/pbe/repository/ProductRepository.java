8e79664e8efa20246a02b0a4a00537e04828935b
rinya <rinya1608@mail.ru>
2020-08-22 00:32:06 +0500

    add findByNameLike and findAllByDiscountPercentNotNull methods

--- src/main/java/ru/pozitron/pbe/repository/ProductRepository.java
+++ src/main/java/ru/pozitron/pbe/repository/ProductRepository.java
@@ -1,14 +1,17 @@
 package ru.pozitron.pbe.repository;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.pozitron.pbe.domain.Category;
 import ru.pozitron.pbe.domain.Product;
 
 import org.springframework.data.domain.Pageable;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+import java.util.ArrayList;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategory(Category category, Pageable pageable);
-    Product findByName(String name);
+    Page<Product> findByNameLike(String name, Pageable pageable);
+    ArrayList<Product> findAllByDiscountPercentNotNull();
     Page<Product> findAll(Pageable pageable);
 }
