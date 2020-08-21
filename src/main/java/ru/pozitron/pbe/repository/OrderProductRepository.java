2e48b13bf030ed41521751e4b285090174248613
rinya <rinya1608@mail.ru>
2020-08-22 00:23:56 +0500

    change extends on JpaRepository

--- src/main/java/ru/pozitron/pbe/repository/OrderProductRepository.java
+++ src/main/java/ru/pozitron/pbe/repository/OrderProductRepository.java
@@ -1,7 +1,8 @@
 package ru.pozitron.pbe.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import ru.pozitron.pbe.domain.OrderProduct;
 
-public interface OrderProductRepository extends CrudRepository<OrderProduct, Long> {
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
 }
