227b11ec402b2f928b621d1feed6052bfa2c7005
rinya <rinya1608@mail.ru>
2020-08-22 00:06:28 +0500

    change extends on JpaRepository and add findByUser method

--- src/main/java/ru/pozitron/pbe/repository/CartRepository.java
+++ src/main/java/ru/pozitron/pbe/repository/CartRepository.java
@@ -1,8 +1,11 @@
 package ru.pozitron.pbe.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import ru.pozitron.pbe.domain.Cart;
+import ru.pozitron.pbe.domain.OrderProduct;
+import ru.pozitron.pbe.domain.User;
 
-public interface CartRepository extends CrudRepository<Cart, Long> {
-
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByUser(User user);
 }
