06935ec68adfeb07091ac8da2634b02947414e04
rinya <rinya1608@mail.ru>
2020-09-16 20:15:38 +0500

    add CartCriteria

---
+++ src/main/java/ru/pozitron/pbe/domain/CartCriteria.java
@@ -0,0 +1,13 @@
+package ru.pozitron.pbe.domain;
+
+public class CartCriteria {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
