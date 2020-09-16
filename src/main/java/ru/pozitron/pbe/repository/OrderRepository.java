e488ce42eaab9f49d4d57b6b3a54843121b0c73b
rinya <rinya1608@mail.ru>
2020-09-16 20:21:22 +0500

    del order repository

--- src/main/java/ru/pozitron/pbe/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.pozitron.pbe.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.pozitron.pbe.domain.Order;
-
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-    Iterable<Order> findAllById(Iterable<Long> iterable);
-}
