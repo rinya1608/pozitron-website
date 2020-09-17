559bb6978689e898a5a0a6ff62b4075fa00e953d
rinya <rinya1608@mail.ru>
2020-09-17 22:20:32 +0500

    change on mime type

--- src/main/java/ru/pozitron/pbe/service/MailSenderService.java
+++ src/main/java/ru/pozitron/pbe/service/MailSenderService.java
@@ -4,22 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class MailSenderService {
+
     @Autowired
-    private JavaMailSender mailSender;
+    private JavaMailSender emailSender;
+
 
     @Value("${spring.mail.username}")
     private String username;
-    public void send(String emailTo,String subject,String message){
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
 
-        mailMessage.setFrom(username);
-        mailMessage.setTo(emailTo);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(message);
-        mailSender.send(mailMessage);
+
+    public void sendMimeMessage(String emailTo,String subject,String message) throws MessagingException {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom(username);
+        mimeMessageHelper.setTo(emailTo);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(message);
+        emailSender.send(mimeMessage);
     }
 }
