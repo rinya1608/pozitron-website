802af77ea6b6098f10d871ac90a28a493ed589bd
rinya <rinya1608@mail.ru>
2020-09-17 22:16:20 +0500

    fix

--- src/main/resources/static/script/quantity.js
+++ src/main/resources/static/script/quantity.js
@@ -1,40 +1,30 @@
-
-function Quantity(numberElement,plusElement,minusElement) {
-    this.number = numberElement;
-    this.plus = plusElement;
-    this.minus = minusElement;
-    this.initEvents();
-}
-Quantity.prototype = {
-    initEvents : function() {
-        var number = this.number;
-        var plus = this.plus;
-        var minus = this.minus;
-        var numberMax = +number.attr('max');
-        plus.on('click', function(event){
-            if (number.val() < numberMax){
-                number.val(+number.val() + 1);
-            }
-
-            
-        });
-        minus.on('click', function(event){
-            if (number.val() > 1){
-                number.val(+number.val() - 1);
-            }
-            
-        });
-        number.on('input',function(event){
-            if (number.val() > numberMax){
-                number.val(numberMax);
-            }
-            else if (number.val() < 1 || isNaN(number.val())){
-                number.val(1);
-            }
-        });
-    }
-}
-
-$(function() {
-    var number = new Quantity( $('#input_number'),$('#button_plus'), $('#button_minus'));
+$(document).ready(function () {
+    quantity();
 });
+
+function quantity() {
+    let number = $("input[id^='input_number']");
+    let plus = $('#button_plus');
+    let minus = $('#button_minus');
+    let numberMax = +number.attr('max');
+    plus.on('click', function(event){
+        if (number.val() < numberMax){
+            number.val(+number.val() + 1);
+        }
+
+    });
+    minus.on('click', function(event){
+        if (number.val() > 1){
+            number.val(+number.val() - 1);
+        }
+    });
+    number.on('input',function(event){
+        if (number.val() > numberMax){
+            number.val(numberMax);
+        }
+        else if (number.val() < 1 || isNaN(number.val())){
+            number.val(1);
+        }
+    });
+}
+
