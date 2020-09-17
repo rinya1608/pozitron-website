c057de070b4761bcc235fe5a3d20f2e642b28b6a
rinya <rinya1608@mail.ru>
2020-09-17 22:14:30 +0500

    add jpa methods

--- src/main/java/ru/pozitron/pbe/repository/CodeRepository.java
+++ src/main/java/ru/pozitron/pbe/repository/CodeRepository.java
@@ -1,12 +1,15 @@
 package ru.pozitron.pbe.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.pozitron.pbe.domain.Code;
 import ru.pozitron.pbe.domain.CodeType;
 import ru.pozitron.pbe.domain.User;
 
-public interface CodeRepository extends CrudRepository<Code, Long> {
+public interface CodeRepository extends JpaRepository<Code, Long> {
     Code findByValue(String value);
     Code findByUserAndCodeTypeAndValueNotNull(User user, CodeType codeType);
     Long countByUserAndCodeTypeAndValueNotNull(User user,CodeType codeType);
+    void deleteByUserAndCodeType(User user, CodeType codeType);
+    Code findByUserAndCodeType(User user, CodeType recoveryPassword);
+
 }
