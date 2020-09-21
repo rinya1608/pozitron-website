c938109f340c9dff00bb4b0850e24d0bff6d77ef
rinya1608 <rinya1608@mail.ru>
2020-09-21 14:13:57 +0500

    move the bean getPasswordEncoder to EncryptionConfig class

--- src/main/java/ru/pozitron/pbe/config/WebSecurityConfig.java
+++ src/main/java/ru/pozitron/pbe/config/WebSecurityConfig.java
@@ -1,14 +1,11 @@
 package ru.pozitron.pbe.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.pozitron.pbe.service.UserService;
 
@@ -16,15 +13,14 @@ import ru.pozitron.pbe.service.UserService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new BCryptPasswordEncoder(8);
+    public WebSecurityConfig(UserService userService,PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -34,11 +30,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             "/static/**",
                             "/activate/*",
                             "/message",
-                            "/recoveryPassword/**").permitAll()
+                            "/recoveryPassword/**",
+                            "/uploads/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
                     .loginPage("/login")
+                    .defaultSuccessUrl("/", true)
                     .permitAll()
                 .and()
                     .logout()
