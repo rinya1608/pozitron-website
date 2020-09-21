e4ee1141cf70a351854d58a7f5195ce59736f1a6
rinya1608 <rinya1608@mail.ru>
2020-09-21 14:09:42 +0500

    fix

--- src/main/java/ru/pozitron/pbe/shedulingtasks/ScheduledTasks.java
+++ src/main/java/ru/pozitron/pbe/shedulingtasks/ScheduledTasks.java
@@ -2,7 +2,6 @@ package ru.pozitron.pbe.shedulingtasks;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.pozitron.pbe.domain.Code;
@@ -14,9 +13,12 @@ import java.time.LocalDateTime;
 
 @Component
 public class ScheduledTasks {
-    @Autowired
-    CodeRepository codeRepository;
+    private final CodeRepository codeRepository;
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    public ScheduledTasks(CodeRepository codeRepository) {
+        this.codeRepository = codeRepository;
+    }
 
     @Scheduled(fixedRate = 30000)
     public void delCode(){
