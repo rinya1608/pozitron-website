964bb21bf64e4834b8333e8fd6ba039ca16a74c8
rinya1608 <rinya1608@mail.ru>
2020-09-21 14:16:06 +0500

    add support for mime

--- src/main/java/ru/pozitron/pbe/service/MailSenderService.java
+++ src/main/java/ru/pozitron/pbe/service/MailSenderService.java
@@ -1,8 +1,6 @@
 package ru.pozitron.pbe.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -13,22 +11,25 @@ import javax.mail.internet.MimeMessage;
 @Service
 public class MailSenderService {
 
-    @Autowired
-    private JavaMailSender emailSender;
+    private final JavaMailSender emailSender;
 
 
     @Value("${spring.mail.username}")
     private String username;
 
+    public MailSenderService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
 
     public void sendMimeMessage(String emailTo,String subject,String message) throws MessagingException {
         MimeMessage mimeMessage = emailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true,"UTF-8");
+        
         mimeMessageHelper.setFrom(username);
         mimeMessageHelper.setTo(emailTo);
         mimeMessageHelper.setSubject(subject);
-        mimeMessageHelper.setText(message);
+        mimeMessageHelper.setText(message,true);
         emailSender.send(mimeMessage);
     }
 }
